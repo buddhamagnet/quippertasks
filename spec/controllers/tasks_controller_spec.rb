35bec5a612e179d86b02b873d3bc814a6ab38ebe
buddhamagnet <buddhamagnet@gmail.com>
2012-04-02 21:34:40 +0100

    fleshing out tasks controller spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe TasksController do
+  describe "creating a task" do
+    describe "not signed in" do
+      it "should deny access" do
+        get :new
+        response.should redirect_to root_path
+      end
+    end
+    
+    describe "signed in" do
+      
+    end
+  end
 
 end
