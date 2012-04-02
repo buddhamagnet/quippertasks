4108d8e16d458b9095536a71721c78d32a116cfa
buddhamagnet <buddhamagnet@gmail.com>
2012-04-02 21:34:02 +0100

    added tasks views directory and started on auth before filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+    
+  def authenticate
+    redirect_to root_path unless user_signed_in?
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < ApplicationController
+  before_filter :authenticate
+  
+  def new
+    @task = Task.new
+  end
 end
