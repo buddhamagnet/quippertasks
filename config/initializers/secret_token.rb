260636330f4c5d4215d70052cea8bb5e07fccecb
buddhamagnet <buddhamagnet@gmail.com>
2012-04-02 19:59:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quippertasks::Application.config.secret_token = 'a194a709897beb246c587fd9456e53f30423d36b43fac8bc293c62033dcf39b0727675492ea732c52e3ecc906c3c5fc5b807be168d06d361478855f4b7bd6cbf'
