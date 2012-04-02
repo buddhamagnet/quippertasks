6f172e22923620355cd280ad6ecc707efaac51b7
buddhamagnet <buddhamagnet@gmail.com>
2012-04-02 21:47:08 +0100

    progressing tasks controller spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -7,11 +7,17 @@ describe TasksController do
         get :new
         response.should redirect_to root_path
       end
+      
+      it "should display a flash message" do
+        get :new
+        flash[:notice].should == "Please log in to create a task"
+      end
     end
     
     describe "signed in" do
-      
+      before(:each) do
+        sign_in @user
+      end
     end
   end
-
 end
