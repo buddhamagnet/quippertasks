24c14be2d49db50647da47590f905a1ebedb8ca9
buddhamagnet <buddhamagnet@gmail.com>
2012-04-02 23:07:50 +0100

    added deadline to attr_accessible for task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :deadline
   validates :name, :presence => true
   validates :description, :presence => true
 end
