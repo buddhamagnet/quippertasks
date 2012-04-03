65563b507dd911ec374e06e7130cce5a78dc0f82
buddhamagnet <buddhamagnet@gmail.com>
2012-04-03 13:17:51 +0100

    added validation for deadline on task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,5 @@ class Task < ActiveRecord::Base
   attr_accessible :description, :name, :deadline
   validates :name, :presence => true
   validates :description, :presence => true
+  validates :dealine, :presence => true
 end
